0dc6a378de163385b925cd1b763c718ea8ad5a45
arijiiiitttt🍉 <arijit123roy098@gmail.com>
2026-02-05 21:15:18 +0530

    ist page icons 2 added

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,24 +1,44 @@
-// Home.tsx
 import { useNavigate } from 'react-router-dom';
+import React,{ useState } from 'react';
 
 export const Home = () => {
+
+  const [theme, setTheme ] = useState('light');
+
   const navigate = useNavigate();
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'dark' ? 'light' : 'dark'));
+  };
 
   const handleStartDrawing = async () => {
     const response = await fetch('http://localhost:5000/create-session', { method: 'POST' });
     const data = await response.json();
-    
     navigate(data.url);
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-white">
-      <button 
-        onClick={handleStartDrawing}
-        className="px-6 py-3 bg-blue-500 hover:bg-blue-700 rounded-md transition"
+    <div 
+    className={`flex flex-row items-center justify-center h-screen text-white gap-x-5 ${
+      theme === 'dark' ? 'bg-gray-950' : 'bg-white text-black'
+    }`}>
+      <a
+        className="flex flex-col items-center justify-center cursor-pointer"
+        onClick={handleStartDrawing}>
+       <img src="/images/create.png" alt="Logo" className="w-30 h-30 cursor-pointer" />
+      </a>
+      <a>
+       <img src="/images/Docs.png" alt="Logo" className="w-26 h-26 cursor-pointer" />
+      </a>
+      <a
+      className='cursor-pointer'
       >
-        Create New Board
-      </button>
+       {theme === 'dark' ? (
+      <img src="/images/light.png" alt="Light Theme" className="w-30 h-30 cursor-pointer" onClick={toggleTheme} />
+       ):(
+        <img src="/images/dark.png" alt="Dark Theme" className="w-30 h-30 cursor-pointer" onClick={toggleTheme} />
+       )}
+      </a>
     </div>
   );
 };
